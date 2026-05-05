e9db38a15071a4f312a4cb3b8ab4eb9a6dc84db1
Mack McCall <mackmc9924@gmail.com>
2026-05-04 21:04:44 -0400

    added cookie in server

--- tinysteps/express-backend/server.js
+++ tinysteps/express-backend/server.js
@@ -9,37 +9,42 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-require('dotenv').config();
+require("dotenv").config();
 
-const session = require('express-session');
-const passport = require('passport');
-require('./auth/passport');
-app.use(session({
-    secret: 'your_secret_key',
-    resave: false,
-    saveUninitialized: true
-}));
+const session = require("express-session");
+const passport = require("passport");
+require("./auth/passport");
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET || "your_secret_key",
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            secure: true,
+            sameSite: "none",
+        },
+    }),
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-const cors = require('cors');
+const cors = require("cors");
 app.use(
     cors({
-        origin: process.env.CLIENT_BASE_URL, // Vite frontend URL
-        methods: 'GET,POST,PUT,DELETE',
+        origin: process.env.CLIENT_BASE_URL,
+        methods: "GET,POST,PUT,DELETE",
         credentials: true,
-    })
+    }),
 );
 
-const milestoneRoutes = require('./routes/milestoneRoutes');
-const userRoutes = require('./routes/userRoutes');
-const newsRoutes = require('./routes/newsRoutes');
+const milestoneRoutes = require("./routes/milestoneRoutes");
+const userRoutes = require("./routes/userRoutes");
+const newsRoutes = require("./routes/newsRoutes");
 
-app.use('/milestones', milestoneRoutes);
-app.use('/users', userRoutes);
-app.use('/news', newsRoutes);
-app.use('/auth', require('./auth/authRoute'));
+app.use("/milestones", milestoneRoutes);
+app.use("/users", userRoutes);
+app.use("/news", newsRoutes);
+app.use("/auth", require("./auth/authRoute"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
